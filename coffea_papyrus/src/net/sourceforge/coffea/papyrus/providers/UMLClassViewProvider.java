53f5fd594e8be9fbabe888a3d25136c44c727760
Bérenger Dulac <berenger.dulac@laposte.net>
2014-09-16 23:12:12 +0200

    Very draft of a view provider

---
+++ coffea_papyrus/src/net/sourceforge/coffea/papyrus/providers/UMLClassViewProvider.java
@@ -0,0 +1,7 @@
+package net.sourceforge.coffea.papyrus.providers;
+
+import org.eclipse.papyrus.uml.diagram.clazz.providers.UMLViewProvider;
+
+public class UMLClassViewProvider extends UMLViewProvider {
+
+}
