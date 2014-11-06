b8407f49e0d3b84168f1bd0b5c417506837a2122
Bérenger Dulac <berenger.dulac@laposte.net>
2014-11-06 23:37:03 +0100

    Improving the design of the interface

--- coffea_core/src/net/sourceforge/coffea/uml2/model/IOperationsOwnerService.java
+++ coffea_core/src/net/sourceforge/coffea/uml2/model/IOperationsOwnerService.java
@@ -7,30 +7,19 @@ import org.eclipse.uml2.uml.Operation;
 public interface IOperationsOwnerService extends IGroupService {
 
 	/**
-	 * Returns the list of services for the operations belonging to the
-	 * handled operations owner
-	 * @return List of services handling the operations belonging to the
-	 * handled owner
+	 * @return List of services handling the operations belonging to the 
+	 * owner
 	 */
 	public List<IMethodService> getOperationsServices();
 
 	/**
-	 * Returns an service owned by the handled operation owner given the 
-	 * operation name
+	 * Returns the service for an operation given the operation simple name
 	 * @param n
-	 * Method name
+	 * Method simple name
 	 * @return Service for the method corresponding to the name
 	 * @see IElementService#getSimpleName()
 	 */
 	public IMethodService getOperationService(String n);
-	
-	/**
-	 * Adds service to the list of services handling the operations 
-	 * belonging to the handled owner
-	 * @param methodService
-	 * New service to add to the list
-	 */
-	public void addOperationService(IMethodService methodService);
 	
 	/**
 	 * Creates an operation in the code owned by the handled element
--- coffea_core/src/net/sourceforge/coffea/uml2/model/IPropertiesOwnerService.java
+++ coffea_core/src/net/sourceforge/coffea/uml2/model/IPropertiesOwnerService.java
@@ -8,30 +8,19 @@ import org.eclipse.uml2.uml.Property;
 public interface IPropertiesOwnerService extends IOwnerService {
 
 	/**
-	 * Returns the list of services handling the properties belonging to the
-	 * handled owner
 	 * @return List of services handling the properties belonging to the
-	 * handled owner
+	 * owner
 	 */
 	public List<IAttributeService> getPropertiesServices();
 	
 	/**
-	 * Returns service for a property owned by the handled owner given the 
-	 * property name
+	 * Returns the service for a property given the property simple name
 	 * @param n
 	 * Property simple name
-	 * @return Service corresponding to the name
+	 * @return Service for the attribute corresponding to the name
 	 * @see IElementService#getSimpleName()
 	 */
 	public IAttributeService getPropertyService(String n);
-	
-	/**
-	 * Adds an service to the list of services handling the properties 
-	 * belonging to the handled owner
-	 * @param prH
-	 * New service to add to the list
-	 */
-	public void addPropertyService(IAttributeService prH);
 	
 	/**
 	 * Creates in the code a property owned by the handled owner
